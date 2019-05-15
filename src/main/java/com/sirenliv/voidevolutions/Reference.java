b6346f50f2354401568ee7e6af7887ff562e7c54
Olivia Maziarka <olivia.maziarka@gmail.com>
2019-05-14 22:54:43 -0700

    Creation of Mod Reference File
    Will be used for basic Mod information

---
+++ src/main/java/com/sirenliv/voidevolutions/Reference.java
@@ -0,0 +1,10 @@
+package com.sirenliv.voidevolutions;
+
+public class Reference {
+
+	public static final String MODID = "voidevolutions";
+	public static final String NAME = "Void Evolutions";
+	public static final String VERSION = "0.0.1";
+
+	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12]";
+}
