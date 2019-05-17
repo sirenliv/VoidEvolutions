438be3ef4a1dac075ace87e5a0400d552a91f35a
SirenLiv <olivia.maziarka@gmail.com>
2019-05-16 21:52:51 -0700

    Added Rudimentary Void Genome Tube

--- src/main/java/com/sirenliv/voidevolutions/init/ModItems.java
+++ src/main/java/com/sirenliv/voidevolutions/init/ModItems.java
@@ -4,6 +4,7 @@ import com.sirenliv.voidevolutions.items.ItemBasic;
 import com.sirenliv.voidevolutions.Reference;
 
 import com.sirenliv.voidevolutions.items.ItemPerfectedVoidGenomeTube;
+import com.sirenliv.voidevolutions.items.ItemRudimentaryVoidGenomeTube;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
@@ -19,19 +20,20 @@ public class ModItems {
 	static Item itemEmptyGenomeTube;
 	static Item itemPerfectedVoidGenomeTube;
 	static Item itemBasicGeneticSlurry;
+	static Item itemRudimentaryGenomeTube;
 	static Item itemPerfectedGeneticSlurry;
 	
 	public static void init() {
 		itemEmptyGenomeTube = new ItemBasic("itemEmptyGenomeTube").setCreativeTab(CreativeTabs.MISC).setMaxStackSize(1);
 		itemPerfectedVoidGenomeTube = new ItemPerfectedVoidGenomeTube("itemPerfectedVoidGenomeTube").setCreativeTab(CreativeTabs.MISC).setMaxStackSize(1);
-		itemBasicGeneticSlurry = new ItemBasic("itemBasicGeneticSlurry").setCreativeTab(CreativeTabs.MISC).setMaxStackSize(1);
+		itemRudimentaryGenomeTube = new ItemRudimentaryVoidGenomeTube("itemRudimentaryVoidGenomeTube").setCreativeTab(CreativeTabs.MISC).setMaxStackSize(1);
 		itemPerfectedGeneticSlurry = new ItemBasic("itemPerfectedGeneticSlurry").setCreativeTab(CreativeTabs.MISC).setMaxStackSize(1);
 	}
 	
 	@SubscribeEvent
 	public static void registerItems(RegistryEvent.Register<Item> event) {
 		event.getRegistry().registerAll(itemEmptyGenomeTube);
-		event.getRegistry().registerAll(itemFilledGenomeTube);
+		event.getRegistry().registerAll(itemRudimentaryGenomeTube);
 		event.getRegistry().registerAll(itemPerfectedVoidGenomeTube);
 		event.getRegistry().registerAll(itemPerfectedGeneticSlurry);
 }
@@ -39,7 +41,7 @@ public class ModItems {
 	@SubscribeEvent
 	public static void registerRenders(ModelRegistryEvent event) {
 		registerRender(itemEmptyGenomeTube);
-		registerRender(itemFilledGenomeTube);
+		registerRender(itemRudimentaryGenomeTube);
 		registerRender(itemPerfectedVoidGenomeTube);
 		registerRender(itemPerfectedGeneticSlurry);
 	}
---
+++ src/main/java/com/sirenliv/voidevolutions/items/ItemRudimentaryVoidGenomeTube.java
@@ -0,0 +1,10 @@
+package com.sirenliv.voidevolutions.items;
+
+import net.minecraft.item.Item;
+
+public class ItemRudimentaryVoidGenomeTube extends Item {
+	public ItemRudimentaryVoidGenomeTube(String name) {
+		setUnlocalizedName(name);
+		setRegistryName(name);
+	}
+}
